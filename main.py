5864c7b13022f70a084d0bab1da26e714e312c93
Ярослав <Yagavdv14@yandex.ru>
2024-09-13 00:25:29 +0300

    Добавлена функция добавления книги: add_book(title, author, year)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Создан словарь и очищен для того что бы было изначальное понимание какие поля будут использованы для библиотеки
 library = {
     'title': {
         'author': None,
@@ -16,51 +17,15 @@ def add_book(title, author, year):
         library[title] = {'author': author, 'year': year, 'availability': None}
 
 
-def remove_book(title):
-    if title in library.keys():
-        library.pop(title)
-        print(f'Книга с названием "{title}" удалена!')
-    else:
-        print(f'Книги с названием "{title}" в каталоге нет!')
-
-
-def issue_book(title):
-    library[title]['availability'] = False
-
-
-def return_book(title):
-    library[title]['availability'] = True
-
-
-def find_book(title):
-    if title in library.keys():
-        if library[title]['availability'] is None:
-            status = 'Книга в библиотеке, но ее статус не определен'
-        elif library[title]['availability']:
-            status = 'Книга доступна'
-        else:
-            status = 'Книга выдана'
-        print(f'Книга с названием "{title}"\n'
-              f'Автор: {library[title]['author']}\n'
-              f'Год издания: {library[title]['year']}\n'
-              f'Статус: {status}')
-    else:
-        print(f'Книги с названием "{title}" в каталоге нет!')
-
-
 if __name__ == '__main__':
-    # Раздел добавления книги
-    add_book('ман', 'igor', 1989)
-    add_book('ман', 'igoawetqwetr', 1989)
-    add_book('екуке', 'igoawetqwetr', 1989)
+    # Добавления новой книги
+    add_book('История тестировщика', 'Yaroslav', 1989)
+    print(library)
 
-    # Раздел удаления книги
-    remove_book('екуке')
-    remove_book('qwerqwer')
+    # Добавление книги которая существует с другим автором
+    add_book('История тестировщика', 'Ярослав', 1989)
+    print(library)
 
-    # Раздел поиска книги и изменение ее статуса (не определена, В наличии, На Выдаче)
-    find_book('ман')
-    return_book('ман')
-    find_book('ман')
-    issue_book('ман')
-    find_book('ман')
+    # Добавление еще одной книги для других функций
+    add_book('Иная книга', 'Ярослав', 1989)
+    print(library)
