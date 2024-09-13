309f3043fa833e97d06fe8298b4aa07f85f88693
Ярослав <Yagavdv14@yandex.ru>
2024-09-13 22:10:43 +0300

    Добавлены функции выдача и возврат книги Дополнена функция добавление книги Дополнительная информация в комментариях внутри кода

--- main.py
+++ main.py
@@ -9,21 +9,43 @@ library = {
 library.clear()
 
 
-def add_book(title, author, year):
+def add_book(title, author, year):  # В дальнейшем уберу аргументы,
+    # так как проверку и ввод аргументов удобнее проводить внутри функции
     if title in library.keys():
-        if input('Книга с таким названием существует!\n'
-                 'Если хотите обновить введите "да" (для отмены - что угодно):\n').lower() in ('yes', 'да'):
+        choice = ''
+        while choice not in ['1', '2']:
+            choice = input('Книга с таким названием существует!\n'
+                           'Выберите дальнейшее действие введя цифру:\n'
+                           '1. Обновить информацию о книге\n'
+                           '2. Не вносить изменения\n'
+                           '3. Показать информацию о книге (в разработке)\n')  # здесь будет функция find_book()
+            if choice == '3':
+                print('\nФункция находится в разработке.\n')
+                input('Для возврата в предыдущее меню нажмите ввод (enter)\n')
+        if choice == '1':
             library[title] = {'author': author, 'year': year, 'availability': None}
+            print('Информация о книге обновлена!\n')
+        if choice == '2':
+            print('Изменения не вносились!\n')
     else:
         library[title] = {'author': author, 'year': year, 'availability': None}
+        print('Книга добавлена в каталог  библиотеки!\n')
 
 
 def remove_book(title):
     if title in library.keys():
         library.pop(title)
-        print(f'Книга с названием "{title}" удалена!')
+        print(f'Книга с названием "{title}" удалена!\n')
     else:
-        print(f'Книги с названием "{title}" в каталоге нет!')
+        print(f'Книги с названием "{title}" в каталоге нет!\n')
+
+
+def issue_book(title):
+    library[title]['availability'] = False
+
+
+def return_book(title):
+    library[title]['availability'] = True
 
 
 if __name__ == '__main__':
@@ -46,3 +68,12 @@ if __name__ == '__main__':
     # Повторое удаление книги с именем 'Иная книга'
     remove_book('Иная книга')
     print(library)
+
+    # Выдача книги и ее возврат в плане структуры будут в иерархии функции find_book()
+    # то есть, нашел книгу и три действия: выдать на руки, вернуть на склад или возврат в предыдущее меню
+    issue_book('История тестировщика')
+    print(library)
+    return_book('История тестировщика')
+    print(library)
+    # Приношу извинения за сумбурный код, но просили придерживаться метода последовательной разработки,
+    # а в голове уже финишный код (его наброски)
