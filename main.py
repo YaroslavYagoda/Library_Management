30f12f45e9499c5dd230f8fedc59eb1129a5b4b3
Ярослав <Yagavdv14@yandex.ru>
2024-09-14 14:33:52 +0300

    Обернул в управляющую функцию, функция find_book() выполнена для 3-х вариантов, но 1-й закоменчен

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Создан словарь и очищен для того что бы было изначальное понимание какие поля будут использованы для библиотеки
 library = {
     'title': {
         'author': None,
@@ -7,74 +6,139 @@ library = {
     },
 }
 library.clear()
+add_book_query = 'Книга с таким названием существует!\nВыберите дальнейшее действие введя цифру:\n'
+aad_book_action = ['1. Обновить информацию о книге',
+                   '2. Не вносить изменения',
+                   '3. Показать информацию о книге (в разработке)']
+library_management_query = 'Необходимо выбрать действие введя его цифру:\n'
+library_management_action = ['1. Добавить книгу в каталог (обновить информацию о книге)',
+                             '2. Удалить книгу из каталога',
+                             '3. Найти книгу (в том числе выдать и вернуть)',
+                             '4. Информация о количестве книг в каталоге',
+                             '5. Завершить работу (выход)']
+issue_return_book_query = 'Необходимо выбрать действие введя его цифру:\n'
+issue_return_book_action = ['1. Выдать книгу на руки',
+                            '2. Сдать книгу на склад',
+                            '3. Вернуться в предыдущее меню']
 
 
-def add_book(title, author, year):  # В дальнейшем уберу аргументы,
-    # так как проверку и ввод аргументов удобнее проводить внутри функции
+def choice_book_title_answer_year():
+    print('Раздел добавления (обновления) книги в каталог')
+    title = ''
+    author = ''
+    year = ''
+    while not (''.join(title.split()).isalnum()):
+        title = input('Введите название книги:\n')
+    while not (''.join(author.split()).isalnum()):
+        author = input('Укажите автора:\n')
+    while not year.isdigit():
+        year = input('Укажите год издания:\n')
+    return [title, author, year]
+
+
+def choice_of_answer(query, list_of_action):
+    choice = ''
+    list_of_answer = [str(i + 1) for i in range(len(list_of_action))]
+    while choice not in list_of_answer:
+        print(query)
+        for action in list_of_action:
+            print(action)
+        choice = input()
+    return choice
+
+
+def add_book(info_book):
+    title = info_book[0]
+    author = info_book[1]
+    year = info_book[2]
     if title in library.keys():
-        choice = ''
-        while choice not in ['1', '2']:
-            choice = input('Книга с таким названием существует!\n'
-                           'Выберите дальнейшее действие введя цифру:\n'
-                           '1. Обновить информацию о книге\n'
-                           '2. Не вносить изменения\n'
-                           '3. Показать информацию о книге (в разработке)\n')  # здесь будет функция find_book()
-            if choice == '3':
-                print('\nФункция находится в разработке.\n')
-                input('Для возврата в предыдущее меню нажмите ввод (enter)\n')
-        if choice == '1':
+        choice = choice_of_answer(add_book_query, aad_book_action)
+        if choice == '3':
+            find_book(title, True)
+            return add_book([title, author, year])
+        elif choice == '1':
             library[title] = {'author': author, 'year': year, 'availability': None}
-            print('Информация о книге обновлена!\n')
-        if choice == '2':
-            print('Изменения не вносились!\n')
+            print(f'Информация о книге с названием "{title}" обновлена!\n')
+            input('Для продолжения работы нажмите ввод (enter)\n')
+        elif choice == '2':
+            print(f'Изменения в книгу с названием "{title}" не вносились!\n')
+            input('Для продолжения работы нажмите ввод (enter)\n')
     else:
         library[title] = {'author': author, 'year': year, 'availability': None}
-        print('Книга добавлена в каталог  библиотеки!\n')
+        print(f'Книга с названием "{title}" добавлена в каталог  библиотеки!\n')
+        input('Для продолжения работы нажмите ввод (enter)\n')
 
 
-def remove_book(title):
+def remove_book():
+    print('Раздел удаления книги из каталога')
+    title = input('Введите название книги:\n')
     if title in library.keys():
         library.pop(title)
         print(f'Книга с названием "{title}" удалена!\n')
+        input('Для продолжения работы нажмите ввод (enter)\n')
     else:
         print(f'Книги с названием "{title}" в каталоге нет!\n')
+        input('Для продолжения работы нажмите ввод (enter)\n')
 
 
 def issue_book(title):
     library[title]['availability'] = False
+    print(f'Книга с названием "{title}" выдана на руки!\n')
+    input('Для продолжения работы нажмите ввод (enter)\n')
 
 
 def return_book(title):
     library[title]['availability'] = True
+    print(f'Книга с названием "{title}" возвращена на склад!\n')
+    input('Для продолжения работы нажмите ввод (enter)\n')
+
+
+def find_book(title, in_another_def):
+    if not in_another_def:
+        print('Раздел поиска книги в каталоге')
+        title = input('Введите название книги:\n')
+    if title in library.keys():
+        # if library[title]['availability'] is None:
+        # status = 'Книга в библиотеке, но ее статус не определен'
+        if library[title]['availability']:
+            status = 'Книга доступна'
+        else:
+            status = 'Книга выдана'
+        print(f'Книга: "{title}"\n'
+              f'Автор: {library[title]['author']}\n'
+              f'Год издания: {library[title]['year']}\n'
+              f'Статус: {status}\n')
+        input('Для продолжения работы нажмите ввод (enter)\n')
+
+        if not in_another_def:
+            choice = choice_of_answer(issue_return_book_query, issue_return_book_action)
+            if choice == '1':
+                issue_book(title)
+            elif choice == '2':
+                return_book(title)
+    else:
+        print(f'Книги с названием "{title}" в каталоге нет!\n')
+        input('Для продолжения работы нажмите ввод (enter)\n')
+
+
+def library_management():
+    print('*' * 100 + '\nТерминал библиотекаря\nУчетная запись № 000-217/2F\n')
+    choice = choice_of_answer(library_management_query, library_management_action)
+    if choice == '1':
+        add_book(choice_book_title_answer_year())
+    elif choice == '2':
+        remove_book()
+    elif choice == '3':
+        find_book('', False)
+    elif choice == '4':
+        print(f'\nКниг в каталоге:{len(library)} шт.\n')
+        input('Для продолжения работы нажмите ввод (enter)\n')
+    elif choice == '5':
+        print('Хорошего и продуктивного дня!!!')
+        return 'exit'
 
 
 if __name__ == '__main__':
-    # Добавления новой книги
-    add_book('История тестировщика', 'Yaroslav', 1989)
-    print(library)
-
-    # Добавление книги которая существует с другим автором
-    add_book('История тестировщика', 'Ярослав', 1989)
-    print(library)
-
-    # Добавление еще одной книги для других функций
-    add_book('Иная книга', 'Ярослав', 1989)
-    print(library)
-
-    # Удаление книги с именем 'Иная книга'
-    remove_book('Иная книга')
-    print(library)
-
-    # Повторое удаление книги с именем 'Иная книга'
-    remove_book('Иная книга')
-    print(library)
-
-    # Выдача книги и ее возврат в плане структуры будут в иерархии функции find_book()
-    # то есть, нашел книгу и три действия: выдать на руки, вернуть на склад или возврат в предыдущее меню
-    issue_book('История тестировщика')
-    print(library)
-    return_book('История тестировщика')
-    print(library)
-    # Приношу извинения за сумбурный код, но просили придерживаться метода последовательной разработки,
-    # а в голове уже финишный код (его наброски)
-    # Алексей я обещал делать проще)) но не выходит))
+    ext = ''
+    while ext != 'exit':
+        ext = library_management()
